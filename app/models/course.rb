77bfa472596c673c1b6df28fdb308ee67b30a360
Punams15 <keys2punam@gmail.com>
2025-08-12 01:59:43 -0500

    Implement student_email_list on Course

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,5 +3,18 @@ class Course < ApplicationRecord
   belongs_to :trimester
   has_many :enrollments
 
-  delegate :title, to: :coding_class
-end
+  def student_email_list
+    email_list = []
+    self.enrollments.each do |enrollment|
+      email_list << enrollment.student.email
+    end
+    email_list
+  end
+    
+  end
+
+  
+  
+
+ 
+
