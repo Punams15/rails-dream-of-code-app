7dcb6d55e3569c12dc25ae0ab725922b4416214b
Punams15 <keys2punam@gmail.com>
2025-09-24 01:33:43 -0500

    modified application-controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,29 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  
+  # Admin restriction
+  def require_admin
+    unless session[:role] == 'admin'
+      flash[:alert] = 'You do not have access to that page'
+      redirect_to root_path
+    end
+  end
+
+  # Mentor restriction
+  def require_mentor
+    unless session[:role] == 'mentor'
+      flash[:alert] = 'You do not have access to that page'
+      redirect_to root_path
+    end
+  end
+
+  # Student restriction
+  def require_student
+    unless session[:role] == 'student'
+      flash[:alert] = 'You do not have access to that page'
+      redirect_to root_path
+    end
+  end
 end
+
