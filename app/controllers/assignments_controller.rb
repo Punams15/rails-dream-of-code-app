c6f670c7abd72580b5ab0276e6430e74819d0088
Punams15 <keys2punam@gmail.com>
2025-10-11 18:09:12 -0500

    Corrected User accounts and authentication for admin, mentors and students - updated controllers

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -1,9 +1,13 @@
 class AssignmentsController < ApplicationController
+ before_action :require_login
   before_action :set_assignment, only: %i[ show edit update destroy submit ]
-  before_action :require_mentor, only: %i[ new create edit update destroy ]
-  before_action :require_student, only: %i[ submit ]
 
-  # Students + Mentors
+  # Students create submissions
+  before_action :require_student, only: %i[ new create submit ]
+
+  # Mentors edit/update submissions
+  before_action :require_mentor, only: %i[ edit update ]
+
   def index
     @assignments = Assignment.all
   end
@@ -11,37 +15,38 @@ class AssignmentsController < ApplicationController
   def show
   end
 
-  # Mentors only
+  # Student creates submission
   def new
-    @assignment = Assignment.new
+    @submission = @assignment.submissions.build
   end
 
   def create
-    @assignment = Assignment.new(assignment_params)
-    if @assignment.save
-      redirect_to @assignment, notice: "Assignment created successfully."
+    @submission = @assignment.submissions.build(submission_params)
+    @submission.user = current_user
+
+    if @submission.save
+      redirect_to assignment_path(@assignment), notice: 'Submission created.'
     else
       render :new, status: :unprocessable_entity
     end
   end
 
-  def edit; end
+  # Mentor edits submission
+  def edit
+    @submission = Submission.find(params[:id])
+  end
+
   def update
-    if @assignment.update(assignment_params)
-      redirect_to @assignment, notice: "Assignment updated successfully."
+    @submission = Submission.find(params[:id])
+    if @submission.update(submission_params)
+      redirect_to assignment_path(@submission.assignment), notice: 'Submission updated.'
     else
       render :edit, status: :unprocessable_entity
     end
   end
 
-  def destroy
-    @assignment.destroy
-    redirect_to assignments_path, notice: "Assignment deleted."
-  end
-
-  # Students only
+  # Student submits assignment
   def submit
-    # student submits their work here
     flash[:notice] = "Assignment submitted!"
     redirect_to @assignment
   end
@@ -52,7 +57,8 @@ class AssignmentsController < ApplicationController
     @assignment = Assignment.find(params[:id])
   end
 
-  def assignment_params
-    params.require(:assignment).permit(:title, :description, :due_date, :course_id)
+  def submission_params
+    # Students submit content; mentors may later add grade/feedback separately
+    params.require(:submission).permit(:content, :attachment)
   end
 end
--- app/controllers/trimesters_controller.rb
+++ app/controllers/trimesters_controller.rb
@@ -1,12 +1,13 @@
 class TrimestersController < ApplicationController
-  before_action :require_student  # only students can access
+  before_action :require_login, except: [:index, :show]
+  before_action :require_admin, only: [:new, :create, :edit, :update, :destroy]
+  before_action :set_trimester, only: [:show, :edit, :update, :destroy]
 
   def index
     @trimesters = Trimester.all
   end
 
   def show
-    @trimester = Trimester.find(params[:id])
   end
 
   def new
@@ -16,15 +17,35 @@ class TrimestersController < ApplicationController
   def create
     @trimester = Trimester.new(trimester_params)
     if @trimester.save
-      redirect_to @trimester, notice: "Trimester created successfully."
+      redirect_to @trimester, notice: 'Trimester created.'
     else
       render :new, status: :unprocessable_entity
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @trimester.update(trimester_params)
+      redirect_to @trimester, notice: 'Trimester updated.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @trimester.destroy
+    redirect_to trimesters_path, notice: 'Trimester deleted.'
+  end
+
   private
 
+  def set_trimester
+    @trimester = Trimester.find(params[:id])
+  end
+
   def trimester_params
-    params.require(:trimester).permit(:name, :start_date, :end_date)
+    params.require(:trimester).permit(:name, :application_deadline, :start_date, :end_date)
   end
 end
